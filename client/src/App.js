e064366b879c3cd50f131687ba19aa57b404683e
aminaouini <aminaouini565@gmail.com>
2020-04-13 21:32:00 +0100

    react router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
+      <Router>
 
-      </div>
+        <div className="App">
+          <Switch>
+            <Route path="/about">
+              <div>/about</div>
+            </Route>
+            <Route path="/users">
+              <div>users</div>
+            </Route>
+            <Route path="/">
+              <div>/</div>
+            </Route>
+          </Switch>
+
+        </div>
+      </Router>
     );
   }
 }
