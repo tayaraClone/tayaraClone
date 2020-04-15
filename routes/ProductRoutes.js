9aa35ab8fb5a3a133de68f34e388075576c709f6
aminaouini <aminaouini565@gmail.com>
2020-04-15 23:07:38 +0100

    added route to retreive seller products by id

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -18,7 +18,14 @@ module.exports = (app) => {
         } catch (err) {
             res.status(400).send(err).end()
         }
+    })
 
+    app.get('/sellerProds/:_id', async (req, res) => {
+        let { _id } = req.params
+        Products.find({ _id }, async (err, data) => {
+            if (err) { return res.status(400).send(err).end() }
 
+            res.send({ results: data || [] }).end()
+        })
     })
 }
