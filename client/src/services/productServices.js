9fad44e3f46a7e8cba0ee03ab403391c364e17ac
aminaouini <aminaouini565@gmail.com>
2020-04-17 01:32:38 +0100

    DRY allProducts component

--- client/src/services/productServices.js
+++ client/src/services/productServices.js
@@ -25,13 +25,13 @@ export default {
     allProds: async (onRetreive) => {
         axios.get('http://localhost:5000/allProds')
             .then(res => {
-                let data = {}
                 let { products } = res.data.results;
-                for (let i = 0; i < products.length; i++) {
-                    let product = products[i];
-                    if (!data[product.categorie]) { data[product.categorie] = [product] }
-                    else { data[product.categorie].push(product) };
-                }
+                let data = { allProds: products }
+                // for (let i = 0; i < products.length; i++) {
+                //     let product = products[i];
+                //     if (!data[product.categorie]) { data[product.categorie] = [product] }
+                //     else { data[product.categorie].push(product) };
+                // }
 
                 onRetreive(data);
             })
