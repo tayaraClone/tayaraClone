7590106ac74f0680c09e6a4d4e93b32d1685a269
aminaouini <aminaouini565@gmail.com>
2020-04-17 20:42:12 +0100

    displayed prices to products in home page

--- client/src/components/home/homeComponents/allProducts.js
+++ client/src/components/home/homeComponents/allProducts.js
@@ -6,16 +6,16 @@ class AllProds extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
             "men's fashion": true,
             "women's fashion": true,
             "boy's fashion": true,
-            "video games": true,
             "baby's essentials": true,
+            "video games": true,
             "Art": true,
             "Other": true,
-            allProds: []
-
+            allProds: [],
+            min: 0,
+            max: 18000
         }
     }
 
@@ -25,7 +25,7 @@ class AllProds extends Component {
     }
 
     removeCategory(name) {
-        this.setState({ [name]: this.state[name] });
+        this.setState({ [name]: !this.state[name] });
     }
 
     componentWillMount() {
@@ -39,7 +39,7 @@ class AllProds extends Component {
     render() {
         return (
             <div id="allProducts">
-                <ChooseCategorie />
+                <ChooseCategorie onRemoveCategorie={this.removeCategory.bind(this)} />
 
                 <div id="homeProducts">
                     {
@@ -48,6 +48,7 @@ class AllProds extends Component {
                                 return (
                                     <div key={i} id={product._id} className="allProds" onClick={this.onclick.bind(this)}>
                                         <img src={product.image} className="allProdsImgs" alt="product image" ></img>
+                                        <h3>{product.cost} DT</h3>
                                         <p>Stock: {product.stockCondition} | Delivery: {product.deliveryCondition}</p>
                                         <h4>{product.name}</h4>
                                     </div>
--- client/src/components/home/homeComponents/chooseByCategories.js
+++ client/src/components/home/homeComponents/chooseByCategories.js
@@ -4,6 +4,7 @@ class ChooseCategorie extends Component {
     constructor(props) {
         super(props);
     }
+
     render() {
         return (
             <div id="categories">
@@ -11,19 +12,19 @@ class ChooseCategorie extends Component {
                 <div id="chooseCat">
 
                     <label for="men">Men's Fashion:</label>
-                    <input type="checkbox" name="men's fashion" checked></input>
+                    <input type="checkbox" name="men's fashion" onClick={e => this.props.onRemoveCategorie(e.target.name)} checked></input>
                     <label for="women">Women's Fashion:</label>
-                    <input type="checkbox" name="women's fashion" checked></input>
+                    <input type="checkbox" name="women's fashion" onClick={e => this.props.onRemoveCategorie(e.target.name)} checked></input>
                     <label for="boys">Boy's Fashion:</label>
-                    <input type="checkbox" name="boy's fashion" checked></input>
+                    <input type="checkbox" name="boy's fashion" onClick={e => this.props.onRemoveCategorie(e.target.name)} checked></input>
                     <label for="babys">Baby's Essentials</label>
-                    <input type="checkbox" name="baby's essentials" checked></input>
+                    <input type="checkbox" name="baby's essentials" onClick={e => this.props.onRemoveCategorie(e.target.name)} checked></input>
                     <label for="videoGames">Video Games:</label>
-                    <input type="checkbox" name="video games" checked></input>
+                    <input type="checkbox" name="video games" onClick={e => this.props.onRemoveCategorie(e.target.name)} checked></input>
                     <label for="art">Art:</label>
-                    <input type="checkbox" name="art" checked></input>
+                    <input type="checkbox" name="art" onClick={e => this.props.onRemoveCategorie(e.target.name)} checked></input>
                     <label for="other">Other:</label>
-                    <input type="checkbox" name="other" checked></input>
+                    <input type="checkbox" name="other" onClick={e => this.props.onRemoveCategorie(e.target.name)} checked></input>
                 </div>
             </div>
         )
