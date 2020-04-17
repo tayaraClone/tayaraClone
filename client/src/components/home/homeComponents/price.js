bc016954c1f1a28415926ebe399ce2f22e91a609
aminaouini <aminaouini565@gmail.com>
2020-04-17 22:53:39 +0100

    added style to price budget front

--- client/src/components/home/homeComponents/price.js
+++ client/src/components/home/homeComponents/price.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 class Price extends Component {
     render() {
         return (
-            <div id="budgetInputs">
-                <input type="number" name="min" className="budget"></input>
-                <input type="number" name="max" className="budget"></input>
+            <div>
+                <h4>Price:</h4>
+                <div id="budgetInputs">
+                    <input type="number" name="min" className="budget" placeholder="Min"></input>
+                    <input type="number" name="max" className="budget" placeholder="Max"></input>
+                </div>
             </div>
         )
     }
