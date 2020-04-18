3727f62cc2fbc88237b4c32de81f4b2b538d9c01
aminaouini <aminaouini565@gmail.com>
2020-04-18 15:27:07 +0100

    setup search page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
 } from "react-router-dom";
 import Home from './components/home/home';
 import Signin from './components/loginSignup/signin';
@@ -15,61 +15,61 @@ import NotOpenedAccNav from './components/navbar/notOpenedAcc';
 import MakeProduct from './components/seller/makeNewProduct';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      openedAcc: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            openedAcc: false
+        }
+        this.openAccount = this.openAccount.bind(this);
     }
-    this.openAccount = this.openAccount.bind(this);
-  }
 
-  openAccount() {
-    this.setState({ openedAcc: true });
-  }
+    openAccount() {
+        this.setState({ openedAcc: true });
+    }
 
-  logOut() {
-    localStorage.removeItem('_______________JWT_Token') // remove the token from the localStorage
-    this.setState({ openedAcc: false }); // set openedAcc state to false so it can logs out
-  }
+    logOut() {
+        localStorage.removeItem('_______________JWT_Token') // remove the token from the localStorage
+        this.setState({ openedAcc: false }); // set openedAcc state to false so it can logs out
+    }
 
-  componentWillMount() {
-    if (localStorage.getItem('_______________JWT_Token')) { this.setState({ openedAcc: true }) } // if the is a token it will set openedAcc to true 
-  }
+    componentWillMount() {
+        if (localStorage.getItem('_______________JWT_Token')) { this.setState({ openedAcc: true }) } // if the is a token it will set openedAcc to true 
+    }
 
-  render() {
-    return (
-      <Router>
+    render() {
+        return (
+            <Router>
 
-        <div className="App">
-          {this.state.openedAcc ? <OpenedAccNav logOut={this.logOut.bind(this)} /> : <NotOpenedAccNav />}
+                <div className="App">
+                    {this.state.openedAcc ? <OpenedAccNav logOut={this.logOut.bind(this)} /> : <NotOpenedAccNav />}
 
-          <Switch>
-            <Route exact path="/signin">
-              {!this.state.openedAcc ? <Signin openAccount={this.openAccount} /> : <Redirect to="/myProducts" />}
-            </Route>
-            <Route exact path="/signup">
-              {!this.state.openedAcc ? <Signup openAccount={this.openAccount} /> : <Redirect to="/myProducts" />}
+                    <Switch>
+                        <Route exact path="/signin">
+                            {!this.state.openedAcc ? <Signin openAccount={this.openAccount} /> : <Redirect to="/myProducts" />}
+                        </Route>
+                        <Route exact path="/signup">
+                            {!this.state.openedAcc ? <Signup openAccount={this.openAccount} /> : <Redirect to="/myProducts" />}
 
-            </Route>
-            <Route exact path="/myProducts">
-              {this.state.openedAcc ? <MyProducts /> : <Redirect to="/signin" />}
+                        </Route>
+                        <Route exact path="/myProducts">
+                            {this.state.openedAcc ? <MyProducts /> : <Redirect to="/signin" />}
 
-            </Route>
-            <Route exact path="/makeProduct">
-              {this.state.openedAcc ? <MakeProduct /> : <Redirect to="/" />}
-            </Route>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="*">
-              <h1>THIS PATH IS NOT FOUND !!</h1>
-            </Route>
-          </Switch>
+                        </Route>
+                        <Route exact path="/makeProduct">
+                            {this.state.openedAcc ? <MakeProduct /> : <Redirect to="/" />}
+                        </Route>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="*">
+                            <h1>THIS PATH IS NOT FOUND !!</h1>
+                        </Route>
+                    </Switch>
 
-        </div>
-      </Router>
-    );
-  }
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ client/src/components/home/homeComponents/search.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            search: "",
+            madeSearch: false
+        }
+    }
+    on_change(e) {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    on_click(e) {
+        e.preventDefault()
+        if (this.state.search.length === 0) alert("Search bar is empty")
+        else {
+            sessionStorage.setItem('-----------________search')
+            this.setState({ madeSearch: true });
+        }
+    }
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" name="search" onChange={this.on_change}></input>
+                    <button onClick={this.on_click}></button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Search;
