aa67db21f55fa16b8c07f2002fe97204d92fcf08
aminaouini <aminaouini565@gmail.com>
2020-04-19 05:58:55 +0100

    splited component

--- client/src/components/product/prod.js
+++ client/src/components/product/prod.js
@@ -3,16 +3,16 @@ import React, { Component } from 'react';
 class Prod extends Component {
     constructor(props) {
         super(props)
+
     }
 
     render() {
         return (
             <div>
-                <img src={this.props.product.image} alt="product image"></img>
-                <h3>{this.props.product.cost}</h3>
-                <h3>{this.props.product.name}</h3>
-                <p>Created at: {this.props.product.createAt}</p>
-                <p>Delivery: {this.props.product.deliveryCondition} | Stock: {this.props.product.stockCondition}</p>
+                <img id="prodImg" src={this.props.product.image} alt="product image"></img>
+
+                <p>Delivery: {this.props.product.deliveryCondition}</p>
+                <p>Stock: {this.props.product.stockCondition}</p>
                 <p>{this.props.product.description}</p>
             </div>
         )
---
+++ client/src/components/product/prod1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Prod1 extends Component {
+    render() {
+        return (
+            <div>
+                <h3>{this.props.product.cost} DT</h3>
+                <h3>{this.props.product.name}</h3>
+                <p>Created at: {this.props.product.createAt}</p>
+            </div>
+        )
+    }
+}
