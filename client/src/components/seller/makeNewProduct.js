925e66c1384cbc4a9dbc3087d6208eda4bb7ccaa
aminaouini <aminaouini565@gmail.com>
2020-04-19 20:56:04 +0100

    added documentaion to search component  front

--- client/src/components/seller/makeNewProduct.js
+++ client/src/components/seller/makeNewProduct.js
@@ -51,7 +51,7 @@ class MakeProduct extends Component {
 
 
         } else {
-            productServices.makeProduct(this.state, this.onMadeProduct.bind(this))
+            productServices.makeProduct(this.state, this.onMadeProduct.bind(this)) // 
         }
 
     }
