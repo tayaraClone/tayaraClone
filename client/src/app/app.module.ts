18e36f31ed2c4217ad884b8fd6234e5539b87565
aminaouini <aminaouini565@gmail.com>
2020-04-22 14:28:09 +0100

    added a product class

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,19 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http'
-
+import { HttpClientModule } from '@angular/common/http';
+import { AllProdsComponent } from './components/home/all-prods/all-prods.component'
+import { ClientProdsService } from './services/client-prods.service'
 
 @NgModule({
   declarations: [
     AppComponent,
+    AllProdsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [ClientProdsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/components/home/all-prods/all-prods.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-prods',
+  templateUrl: './all-prods.component.html',
+  styleUrls: ['./all-prods.component.css']
+})
+export class AllProdsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
