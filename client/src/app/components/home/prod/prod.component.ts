1538595c072d95b6d308b3bbe8a1557cadf1bf1d
aminaouini <aminaouini565@gmail.com>
2020-04-22 22:57:38 +0100

    displayed all products

--- client/src/app/components/home/prod/prod.component.ts
+++ client/src/app/components/home/prod/prod.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../../../classes/product';
 @Component({
   selector: 'app-prod',
   templateUrl: './prod.component.html',
   styleUrls: ['./prod.component.css']
 })
 export class ProdComponent implements OnInit {
-
+  @Input() product: Product;
   constructor() { }
 
   ngOnInit(): void {
