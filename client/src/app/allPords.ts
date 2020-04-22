713c472007ea8a2dcd289969e62e4fa76010ee64
aminaouini <aminaouini565@gmail.com>
2020-04-22 13:12:35 +0100

    removed unnecessary files

--- client/src/app/allPords.ts
+++
@@ -1,9 +0,0 @@
-export interface AllProds {
-    _id: string,
-    name: string,
-    stockCondition: string,
-    image: string,
-    cost: number,
-    categorie: string,
-    deliveryCondition: string
-}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,24 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { DisplayAllCsComponent } from './display-all-cs/display-all-cs.component';
-import { AllProdsService } from './all-prods.service';
 import { HttpClientModule } from '@angular/common/http'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    DisplayAllCsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [AllProdsService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
