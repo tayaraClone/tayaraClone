bf92f2017329a3d67945cb7417aea42170f98bce
aminaouini <aminaouini565@gmail.com>
2020-04-24 04:30:47 +0100

    setup routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home/home.component';
+import { SignInComponent } from './components/auth/sign-in/sign-in.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'signin', component: SignInComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
