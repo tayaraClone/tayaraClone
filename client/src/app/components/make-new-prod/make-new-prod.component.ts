7125c67f84bb051c2220c3d46cef553ba3960dd1
aminaouini <aminaouini565@gmail.com>
2020-04-26 00:08:12 +0100

    added save method to ckeck inputs

--- client/src/app/components/make-new-prod/make-new-prod.component.ts
+++ client/src/app/components/make-new-prod/make-new-prod.component.ts
@@ -10,14 +10,51 @@ export class MakeNewProdComponent implements OnInit {
   name: string = "";
   cost: number;
   deliveryCondition: string = "";
+  productPlace: string = "";
+  image: string = "";
+  categorie: string = "";
+  stockCondition: string = "";
+  description: string = "";
+
 
   constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
     this.auth.routeGuard()
+
   }
   onSave() {
-    console.log(this.deliveryCondition, this.name)
+    let urlValidator = /http:\/\//;
+
+    if (this.name === "" ||
+      !urlValidator.test(this.image) ||
+      this.description === "" ||
+      this.categorie === "" ||
+      this.stockCondition === "" ||
+      this.productPlace === "" ||
+      !this.cost ||
+      this.deliveryCondition === "") {
+
+      if (this.name === "") alert("product's name is empty")
+      else if (!urlValidator.test(this.image)) alert("image url is not valid")
+      else if (this.description === "") alert('description is empty')
+      else if (this.categorie === "") alert("categorie is not picked")
+      else if (this.stockCondition === "") alert('stock condition is not picked')
+      else if (this.productPlace === "") alert('product place is empty')
+      else if (!this.cost) alert('cost is empty')
+      else if (this.deliveryCondition === "") alert('delivery condition is not picked')
+    } else {
+      let newProd = {
+        name: this.name,
+        image: this.image,
+        description: this.description,
+        categorie: this.categorie,
+        stockCondition: this.stockCondition,
+        deliveryCondition: this.deliveryCondition,
+        cost: this.cost,
+        productPlace: this.productPlace
+      }
+    }
   }
 
 }
