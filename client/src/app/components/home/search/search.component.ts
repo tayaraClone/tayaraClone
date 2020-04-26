5928c3fb78f97e69fa31f82a97d2872a99adccdd
aminaouini <aminaouini565@gmail.com>
2020-04-26 18:49:11 +0100

    added search method

--- client/src/app/components/home/search/search.component.ts
+++ client/src/app/components/home/search/search.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
+  search: string = "";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSearch() {
+    if (this.search === "") alert('Search bar is empty')
+    else {
+
+    }
   }
 
 }
